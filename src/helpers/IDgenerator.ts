a9052084b7206eb1a9e21c181bb3fd66fdbffb5a
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2022-06-21 09:34:04 +0400

    feat: delete unused else condition

--- src/helpers/IDgenerator.ts
+++ src/helpers/IDgenerator.ts
@@ -1,7 +1,7 @@
 export default function IDgenerator(arr: any): number {
   if (arr.length === 0) {
     return 1;
-  } else {
-    return arr[arr.length - 1].id + 1;
   }
+  
+  return arr[arr.length - 1].id + 1;
 }
