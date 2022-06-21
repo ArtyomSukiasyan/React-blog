959b05bf5943e05c6eacd8b50c9fcb1f001d7025
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2022-06-21 22:09:25 +0400

    feat: add post delete functional

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,9 +1,26 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
+import { ReactComponent as ReactLogo } from "../../Assets/remove.svg";
 import { IPost } from "../../models/Post";
 import "./Home.scss";
 
 export default function Home(): ReactElement {
-  const posts = JSON.parse(localStorage.getItem("posts") || "[]");
+  const [posts, setPosts] = useState(
+    JSON.parse(localStorage.getItem("posts") || "[]")
+  );
+
+  const deletePost = (postUserId: number, postId: number) => {
+    const currentUser = JSON.parse(localStorage.getItem("currentUser") || "[]");
+    const currentUserId = currentUser[0].id;
+
+    if (postUserId !== currentUserId) {
+      return;
+    }
+
+    const filteredPosts = posts.filter((el: any) => el.id !== postId);
+    setPosts(filteredPosts);
+    localStorage.setItem("posts", JSON.stringify(filteredPosts));
+  };
+
   return (
     <div className="container">
       {posts.map((post: IPost) => {
@@ -11,6 +28,10 @@ export default function Home(): ReactElement {
           <div key={post.id} className="posts">
             <div className="title">
               <h2>{post.title}</h2>
+              <ReactLogo
+                onClick={() => deletePost(post.userId, post.id)}
+                className="delete-icon"
+              />
             </div>
             <div className="content">{post.info}</div>
           </div>
