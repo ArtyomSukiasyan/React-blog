882fc14cec7f262f14086ee146d2c3a4ae0ce51b
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2022-06-21 11:53:02 +0400

    feat: add new class for button component

--- src/Components/Header/Navbar/Navbar.tsx
+++ src/Components/Header/Navbar/Navbar.tsx
@@ -1,13 +1,12 @@
 import { ReactElement } from "react";
 import { useNavigate } from "react-router-dom";
 import NavbarItem from "./NavbarItem/NavbarItem";
-import "./Navbar.scss";
 import Button from "../../Button/Button";
 
 export default function Navbar(): ReactElement {
   const navigate = useNavigate();
 
-  const Logout = () => {
+  const Logout = (): void => {
     localStorage.removeItem("currentUser");
     navigate("/");
   };
@@ -17,7 +16,7 @@ export default function Navbar(): ReactElement {
       <NavbarItem href="/" title="Home" />
       <NavbarItem href="/create-post" title="Create post" />
       {localStorage.getItem("currentUser") ? (
-        <Button className="log-out" onClick={Logout} title="log out" />
+        <Button className="log-out" onClick={Logout} title="Log out" />
       ) : (
         <>
           <NavbarItem href="/sign-in" title="Sign in" />
