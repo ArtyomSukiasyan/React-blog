aa16b00ecebea232e874450bc42a5740a6b61cb5
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2022-06-21 21:05:52 +0400

    feat: add link to home page for logo

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -2,11 +2,18 @@ import Navbar from "./Navbar/Navbar";
 import logo from "../../Assets/logo.png";
 import "./Header.scss";
 import { ReactElement } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Header(): ReactElement {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/");
+  };
+
   return (
     <header>
-      <img className="logo" src={logo} alt="Logo" />
+      <img className="logo" src={logo} alt="Logo" onClick={handleClick} />
       <Navbar />
     </header>
   );
