c71ebd17c1bec5c8453183182cabedcf0c72d841
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2022-06-21 09:35:48 +0400

    feat: add types for Input component

--- src/Components/Input/Input.tsx
+++ src/Components/Input/Input.tsx
@@ -1,5 +1,6 @@
+import { ReactElement } from "react";
 import { IInput } from "../../models/Input";
 
-export default function Input({ type, placeholder, onChange }: IInput) {
+export default function Input({ type, placeholder, onChange }: IInput): ReactElement {
   return <input type={type} placeholder={placeholder} onChange={onChange} />;
 }
