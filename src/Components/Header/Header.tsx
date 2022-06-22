f00b3bd3c2356fccaf2a26dca757e9314dfb16c8
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2022-06-22 10:51:06 +0400

    feat: add mobile menu

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import Navbar from "./Navbar/Navbar";
 import logo from "../../Assets/logo.png";
 import "./Header.scss";
+import MobileMenu from "../MobileMenu/MobileMenu";
 
 export default function Header(): ReactElement {
   const navigate = useNavigate();
@@ -14,7 +15,12 @@ export default function Header(): ReactElement {
   return (
     <header>
       <img className="logo" src={logo} alt="Logo" onClick={handleClick} />
-      <Navbar />
+      <nav className="desktop">
+        <Navbar />
+      </nav>
+      <nav className="mobile">
+        <MobileMenu />
+      </nav>
     </header>
   );
 }
---
+++ src/Components/MobileMenu/MobileMenu.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { ReactComponent as BuregMenu } from "../../Assets/burger-menu.svg";
+import { ReactComponent as CloseIcon } from "../../Assets/close.svg";
+import "./MobileMenu.scss";
+
+export default function MobileMenu() {
+  const [isOpen, setIsopen] = useState(false);
+  const navigate = useNavigate();
+
+  const toggleMobileMenu = (): void => {
+    setIsopen(!isOpen);
+  };
+
+  const Logout = (): void => {
+    localStorage.removeItem("currentUser");
+    navigate("/");
+  };
+
+  return (
+    <>
+      <BuregMenu className="burger-menu" onClick={toggleMobileMenu} />
+      <div className={`sidebar ${isOpen && "active"}`}>
+        <CloseIcon className="close-icon" onClick={toggleMobileMenu} />
+        <NavLink className="mobile-link" to="/">
+          Home
+        </NavLink>
+        <NavLink className="mobile-link" to="/create-post">
+          Create Post
+        </NavLink>
+
+        {localStorage.getItem("currentUser") ? (
+          <span className="mobile-link" onClick={Logout}>
+            Log Out
+          </span>
+        ) : (
+          <>
+            <NavLink className="mobile-link" to="/sign-in">
+              Sign in
+            </NavLink>
+            <NavLink className="mobile-link" to="/login">
+              Log In
+            </NavLink>
+          </>
+        )}
+      </div>
+      <div className={`${isOpen && "active"}`} onClick={toggleMobileMenu}></div>
+    </>
+  );
+}
